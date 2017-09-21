0ea6b37d1bfdaa821c9d39577ac9cae5fa80a1eb
alex-gherega <alex.gherega@gmail.com>
2017-09-21 17:00:14 +0300

    New project under web-utils-2.0: Analysis

---
+++ analysis/project.clj
@@ -0,0 +1,9 @@
+(defproject analysis "web-utils-2.0/0.1.0-alpha"
+  :description "Tools for various analysis"
+  :url "https://github.com/alex-gherega/web-utils-2.0/tree/master/analysis"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [clj-http "3.7.0"]
+                 [hiccup "1.0.5"]
+                 [hickory "0.7.1"]])
---
+++ analysis/src/analysis/web_page_test.clj
@@ -0,0 +1,45 @@
+(ns analysis.web-page-test
+  (:require [hiccup.core :refer [html]]
+            [hickory.core :refer [parse as-hiccup]]
+            [clj-http.client :refer [get]]))
+
+(def objective-values {:load-time "LoadTime"
+                       :first-byte "TTFB"
+                       :start-render "StartRender"
+                       :visual-complete "VisualComplete"
+                       :speed-index "SpeedIndex"
+                       :fst-interactive "FirstInteractive"
+                       :doc-complete "DocComplete"
+                       :fully-loaded "FullyLoaded"
+                       :bytes-in "BytesIn"})
+
+;; e.g. url: https://www.webpagetest.org/result/170920_EE_90884ce83af9264e0604cdcf28b625d9/
+(defn wget-details [url run]
+  (get (str url run "/details/")))
+
+(defn- lookup-pattern [objective]
+  (html [:td {:id (objective-values objective)} "*.*"]))
+
+(defn extract-pattern
+
+  ([url run objective]
+   (extract-pattern ((wget-details url run) :body)
+                    objective))
+  
+  ([page-body objective]
+   (-> objective
+       lookup-pattern
+       re-pattern
+       (re-find page-body))))
+
+(defn extract-value
+  ([url run objective]
+   (extract-value (extract-pattern url run objective) objective))
+
+  ([str-pattern objective]
+   [objective (or (re-find #"\d\.\d+\s*\w" str-pattern)
+                  (re-find #"\d,\d+\s*.*B" str-pattern))]))
+
+;; e.g. usage: (analysis.web-page-test/extract-value "https://www.webpagetest.org/result/170920_EE_90884ce83af9264e0604cdcf28b625d9/" 1 :bytes-in)
+
+

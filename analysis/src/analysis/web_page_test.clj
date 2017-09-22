e4c2646203fd8b6bf3f2da0c7af2474e0a1c76d8
alex-gherega <alex.gherega@gmail.com>
2017-09-22 13:27:58 +0300

    solving bug

--- analysis/src/analysis/web_page_test.clj
+++ analysis/src/analysis/web_page_test.clj
@@ -1,8 +1,10 @@
 (ns analysis.web-page-test
   (:require [hiccup.core :refer [html]]
             [hickory.core :refer [parse as-hiccup]]
-            [clj-http.client :refer [get]]))
+            [clj-http.client :refer [get]]
+            [clojure.spec.alpha :as s]))
 
+;; structures .....................
 (def ^:dynamic objective-values {:load-time "LoadTime"
                                  :first-byte "TTFB"
                                  :start-render "StartRender"
@@ -12,6 +14,13 @@
                                  :doc-complete "DocComplete"
                                  :fully-loaded "FullyLoaded"
                                  :bytes-in "BytesIn"})
+
+(s/def ::env (s/and vector?  (s/coll-of (s/tuple string? string?))))
+(s/def ::unit string?)
+(s/def ::values (s/and vector? (s/coll-of number?)))
+(s/def ::objective (-> objective-values keys set))
+(s/def ::browser (s/or :name string? :type #{"Firefox" "Chrome" "IE11"}))
+(s/def ::aggregate-result (s/keys :req [::env ::browser ::objective ::values ::unit]))
 
 ;; utilities .....................
 ;; e.g. url: https://www.webpagetest.org/result/170920_EE_90884ce83af9264e0604cdcf28b625d9/
@@ -33,7 +42,11 @@
        as-hiccup
        flatten
        (filter string?)
+       rest
        (apply str)))
+
+(defn extract-browser [environment-str]
+  (second (clojure.string/split environment-str #"-")))
 
 (defn sanitize [string-value]
   {:value (->> (clojure.string/replace string-value "," ".")
@@ -49,7 +62,6 @@
                     lookup-fn))
   
   ([page-body objective lookup-fn]
-   (prn (lookup-fn objective))
    (-> objective
        lookup-fn
        re-pattern
@@ -60,11 +72,19 @@
    (extract-value (extract-pattern url run objective lookup-fn) objective))
   
   ([url run objective]
-   (extract-value (extract-pattern url run objective lookup-location-pattern) objective))
+   (extract-value (extract-pattern url run objective lookup-objective-pattern) objective))
 
   ([str-pattern objective]
+   (prn str-pattern)
    [objective (or (re-find #"\d,\d+\s*.*B" str-pattern)
                   (re-find #"\d\.??\d+\s*\w*" str-pattern)
                   (extract-location str-pattern))]))
 
 ;; e.g. usage: (analysis.web-page-test/extract-value "https://www.webpagetest.org/result/170920_EE_90884ce83af9264e0604cdcf28b625d9/" 1 :bytes-in)
+
+
+;; aggregators .....................
+(defn agg-basic-result [url run objective]
+  (let [location (extract-value url run "From:" lookup-location-pattern)
+        browser (-> location second extract-browser)
+        ]))
